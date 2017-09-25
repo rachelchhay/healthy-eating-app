d26b571735021b465fc4ff2bef4fb3bf782d475c
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-09-25 16:08:43 -0500

    Search form added, react bootsrap installed

--- src/SearchRecipe.js
+++ src/SearchRecipe.js
@@ -1,10 +1,29 @@
 import React, { Component }  from 'react';
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+import './index.css';
+
 
 class SearchRecipe extends Component {
   render() {
     return (
-      <div>
+      <div className='search'>
         <h1>Find A Healthy Recipe</h1>
+
+        <Form inline>
+          <FormGroup>
+            <ControlLabel>Ingredients:</ControlLabel>
+            {' '}
+            <FormControl type="text" placeholder="Ex. chicken, pasta" />
+          </FormGroup>
+          {' '}
+          <FormGroup>
+            <ControlLabel>Dish:</ControlLabel>
+            {' '}
+            <FormControl type="text" placeholder="Ex. lasagna"></FormControl>
+          </FormGroup>
+          {' '}
+          <Button>Submit</Button>
+        </Form>
       </div>
     )
   }
