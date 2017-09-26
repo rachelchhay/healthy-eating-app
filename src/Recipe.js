3dbfeff127cb692c913b41e0525833f929f4717d
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-09-26 15:49:03 -0500

    Change key property

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,7 +4,7 @@ import EditRecipe from './EditRecipe';
 // add state
 class Recipe extends Component {
   render() {
-    let recipe = this.props.data.map(post => {
+    let recipe = this.props.data.map((post, i) => {
       return (
         <EditRecipe
         title={post.title}
@@ -14,7 +14,7 @@ class Recipe extends Component {
         directions={post.directions}
         author={post.author}
         uniqueID={ post['_id'] }
-        key={ post['_id'] }
+        key={ i }
         onPostDelete={ this.props.onPostDelete }
         onPostUpdate={ this.props.onPostUpdate } >
         </EditRecipe>
